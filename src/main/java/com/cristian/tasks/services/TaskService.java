499210d3421b53b684cddaf99f39e3e3b80313db
CriFigShe <crifigshe@gmail.com>
2025-07-27 18:56:33 +0200

    Get a single task

--- src/main/java/com/cristian/tasks/services/TaskService.java
+++ src/main/java/com/cristian/tasks/services/TaskService.java
@@ -3,9 +3,11 @@ package com.cristian.tasks.services;
 import com.cristian.tasks.domain.entities.Task;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface TaskService {
     List<Task> listTasks(UUID taskListId);
     Task createTask(UUID taskListId, Task task);
+    Optional<Task> getTask(UUID taskListId, UUID taskId);
 }
